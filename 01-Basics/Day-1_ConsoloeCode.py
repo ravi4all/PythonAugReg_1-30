98626966a5d662b46ca50875323dbf2d9ed29f83
ravi4all <ravikanttyagi.2295@gmail.com>
2017-08-14 15:26:22 +0530

    Add files via upload

---
+++ 01-Basics/Day-1_ConsoloeCode.py
@@ -0,0 +1,79 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
+>>> a = 10
+>>> b = 12
+>>> a+b
+22
+>>> print(a+b)
+22
+>>> a-b
+-2
+>>> a/b
+0.8333333333333334
+>>> a*b
+120
+>>> a = 10.5
+>>> type(a)
+<class 'float'>
+>>> a=10
+>>> type(a)
+<class 'int'>
+>>> a = "Hello World"
+>>> a+b
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    a+b
+TypeError: must be str, not int
+>>> a*b
+'Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World'
+>>> "Hello"*5
+'HelloHelloHelloHelloHello'
+>>> a,b = 10,12
+>>> a
+10
+>>> b
+12
+>>> a,b = b,a
+>>> a
+12
+>>> b
+10
+>>> a = 14
+>>> b = 12
+>>> a/b
+1.1666666666666667
+>>> a//b
+1
+>>> print "Hello"
+SyntaxError: Missing parentheses in call to 'print'
+>>> a = 12
+>>> b = 13
+>>> a/b
+0.9230769230769231
+>>> a = 3
+>>> a^10
+9
+>>> a**8
+6561
+>>> a**10
+59049
+>>> a = "Hello Python"
+>>> a[0]
+'H'
+>>> a[2]
+'l'
+>>> a[0:6]
+'Hello '
+>>> a[0:5]
+'Hello'
+>>> a[-1]
+'n'
+>>> a[-2]
+'o'
+>>> a[:-1]
+'Hello Pytho'
+>>> a[::-1]
+'nohtyP olleH'
+>>> 
