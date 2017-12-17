966277c39ed303cfb3ddbdf43e2945e55bb3397d
ravi4all <ravikanttyagi.2295@gmail.com>
2017-12-17 17:17:57 +0530

    Add files via upload

---
+++ 01-Code/02-ConnectingToServer.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+# ip = socket.gethostbyname('www.brain-mentors.com')
+# print(ip)
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket successfully created")
+
+except socket.error as err:
+    print("Error :",err)
+
+# default port for socket
+port = 80
+
+try:
+    host_ip = socket.gethostbyname('www.google.com')
+
+except socket.gaierror:
+    print("Error occurred")
+    sys.exit()
+
+# Connecting to server
+s.connect((host_ip, port))
+
+print("Successfully connected to google on port",host_ip)
